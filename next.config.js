25d712660d275fd35eac9f2cadfccef15426bf59
Tacianjr07 <t.juniior13@gmail.com>
2024-01-16 17:07:27 -0300

    add domains pexels

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ["image/avif", "image/webp"],
+    domains: ["images.pexels.com"],
+  },
+  compiler: {
+    styledComponents: true,
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
+};
