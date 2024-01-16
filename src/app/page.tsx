eb755c85e18df2b1301594d9b8a187c4c5de4b91
Tacianjr07 <t.juniior13@gmail.com>
2024-01-16 17:05:39 -0300

    :lipstick: feat: style page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import * as Styles from "./style";
 export default function Home() {
   return (
     <Styles.Container>
-      <header>
+      <Styles.Header>
         <h1>CHAT.IO </h1>
-        <ul>
-          <Link href={"#"}></Link>
-          <Link href={"#"}></Link>
-        </ul>
-      </header>
-      <main>
+        <Styles.List>
+          <Styles.ListItem href={"#"}>Login</Styles.ListItem>
+          <Styles.ListItem href={"#"}>Register</Styles.ListItem>
+        </Styles.List>
+      </Styles.Header>
+      <Styles.Main>
         <section>
           <h2>Have your</h2>
           <h2>best chat</h2>
@@ -22,10 +22,22 @@ export default function Home() {
 
           <button>Try it free</button>
         </section>
-        <aside>
-          <Image />
-        </aside>
-      </main>
+        <Styles.Aside>
+          <Image
+            src={
+              "https://images.pexels.com/photos/4492134/pexels-photo-4492134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            }
+            width={100}
+            height={100}
+            alt="talking"
+            quality={100}
+          />
+          <p>
+            have fun chatting with your friends and family with our app you can
+            send emojis and photos come create your account and enjoy
+          </p>
+        </Styles.Aside>
+      </Styles.Main>
     </Styles.Container>
   );
 }
--- src/app/style.ts
+++ src/app/style.ts
@@ -1,9 +1,64 @@
 import { ColorsTheme } from "@/styles/theme";
 import styled from "styled-components";
+import css from "styled-jsx/css";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: ${({ theme }) => ColorsTheme.colors.primary};
   color: #fff;
+  padding: 16px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 120px;
+  padding: 26px;
+
+  h1 {
+    font-weight: 900;
+  }
+`;
+
+type ButtonProps = {
+  IsButtonLogin?: boolean;
+};
+
+export const List = styled.ul`
+  display: flex;
+  gap: 15px;
+`;
+
+export const ListItem = styled.a<ButtonProps>`
+  gap: 15px;
+  text-decoration: none;
+  color: blue;
+  background-color: ${(props) => (props.IsButtonLogin ? "#3574C0" : "#fff")};
+  padding: 10px 14px;
+  border-radius: 8px;
+  font-size: larger;
+  font-weight: 500;
+  transition: all 0.4s;
+
+  &:hover {
+    background: #3574;
+    color: white;
+  }
+`;
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 26px;
+`;
+
+export const Aside = styled.aside`
+  img {
+    height: 330px;
+    width: 100%;
+    margin-bottom: 10px;
+  }
 `;
