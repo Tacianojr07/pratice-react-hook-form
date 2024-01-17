751e9d7898d9b790f84519e727d319394733d25e
Tacianjr07 <t.juniior13@gmail.com>
2024-01-17 17:14:28 -0300

    :sparkles: feat: using react-hook-form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,23 +4,58 @@ import { FaUser } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import InputIcon from "../../../components/InputIcon";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { DevTool } from "@hookform/devtools";
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 function Login() {
+  const form = useForm<FormValues>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const { register, handleSubmit, getValues, control, formState } = form;
+  const { errors } = formState;
+
+  function oSbumit(data: FormValues) {
+    console.log(data);
+  }
+
   return (
     <Styles.Container>
-      <Styles.Form>
+      <Styles.Form onSubmit={handleSubmit(oSbumit)}>
         <div>
-          <label htmlFor="">Username</label>
-          <InputIcon type="text" placeholder="digite seu email" />
+          <label htmlFor="email">Email</label>
+          <Styles.Input
+            id="email"
+            {...register("email")}
+            type="text"
+            placeholder="digite seu email"
+          />
+          <p style={{ color: "red" }}>{errors.email?.message}</p>
         </div>
         <div>
-          <label htmlFor="">Password</label>
-          <InputIcon type="password" placeholder="digite sua senha" />
+          <label htmlFor="password">Password</label>
+          <Styles.Input
+            id="password"
+            {...register("password")}
+            type="password"
+            placeholder="digite sua senha"
+          />
+          <p style={{ color: "red" }}>{errors.password?.message}</p>
         </div>
         <button>Login</button>
 
         <Link href={"#"}>Esqueceu senha?</Link>
       </Styles.Form>
+      <DevTool control={control} />
     </Styles.Container>
   );
 }
